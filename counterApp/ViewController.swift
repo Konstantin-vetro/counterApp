557f7f79c5a4cb6f6977df6edd92864ca9fc6f15
Konstantin-vetro <konstantin.vetro@yandex.ru>
2023-02-12 03:33:39 +0500

    Update ViewController.swift
    Создал аутлет Label, аутлеты Button1,2, экшены на Button1,2. Во viewDidLoad задал начальные значения

--- counterApp/ViewController.swift
+++ counterApp/ViewController.swift
@@ -8,10 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var count: Int = 0
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet weak var someButton: UIButton!
+    
+    @IBAction func actionButton(_ sender: Any) {
+        count += 1
+        textLabel.text = "Значение счётчика: \(count)"
+    }
+    
+    @IBOutlet weak var eraseButton: UIButton!
+    
+    @IBAction func eraseValueButton(_ sender: Any) {
+        count = 0
+        textLabel.text = "Значение счётчика: \(count)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textLabel.text = "Значение счётчика: \(count)"
+        
+        someButton.backgroundColor = .gray
+        someButton.tintColor = .white
+        someButton.setTitle("Click me", for: .normal)
+        
+        eraseButton.backgroundColor = .red
+        eraseButton.tintColor = .white
+        eraseButton.setTitle("Очистить", for: .normal)
     }
 
 
